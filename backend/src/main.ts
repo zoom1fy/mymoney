c8b1dd3025c5898b9ec3b8d3616dde76a57c63d4
Ivan <yutub.lol@yandex.ru>
2025-07-25 02:07:38 +0300

    solved the problem with docker not picking up cookie-parser + changed the node ver to 20

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
