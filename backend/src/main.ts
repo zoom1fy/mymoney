18217373f885e50d717d731000645a797b87f586
Ivan <yutub.lol@yandex.ru>
2026-01-08 03:38:32 +0300

    change cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'http://localhost:3001', // фронтенд
+    origin: true,
     credentials: true, // чтобы сработал withCredentials
   });
 
