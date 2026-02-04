331c89057f36df1e23306f3d08592bb8a6f0c686
Ivan <yutub.lol@yandex.ru>
2026-02-05 01:41:47 +0300

    remove comment bout font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,9 @@
 import './globals.css'
 import { Providers } from './providers'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from 'sonner'
 
 import { SITE_NAME } from '@/constants/seo.constants'
-
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin']
-// })
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin']
-// })
 
 export const metadata: Metadata = {
   title: {
