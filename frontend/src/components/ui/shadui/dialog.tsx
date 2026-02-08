4c55bcc069cc06b261c52e1a03720796c0e21fd2
Ivan <yutub.lol@yandex.ru>
2026-02-09 00:57:31 +0300

    fix ui bug with modal

--- frontend/src/components/ui/shadui/dialog.tsx
+++ frontend/src/components/ui/shadui/dialog.tsx
@@ -57,7 +57,21 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50',
+        'fixed inset-0 z-50',
+
+        // blur ВСЕГДА есть
+        'bg-black/30 backdrop-blur-md',
+
+        // анимация без "пустого кадра"
+        'data-[state=open]:animate-in',
+        'data-[state=closed]:animate-out',
+        'data-[state=open]:fade-in-0',
+        'data-[state=closed]:fade-out-0',
+
+        // не убираем blur при closed
+        'data-[state=open]:backdrop-blur-md',
+        'data-[state=closed]:backdrop-blur-md',
+
         className
       )}
       {...props}
@@ -88,7 +102,7 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-8"
           >
             <XIcon />
             <span className="sr-only">Close</span>
