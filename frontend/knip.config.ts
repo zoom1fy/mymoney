0195e85835d5839f6fa6768eaf832909824a31e9
Ivan <yutub.lol@yandex.ru>
2026-05-11 14:46:09 +0300

    update knip conf + ver

--- frontend/knip.config.ts
+++ frontend/knip.config.ts
@@ -4,14 +4,17 @@ import type { KnipConfig } from 'knip'
 const config: KnipConfig = {
   entry: [
     'next.config.js',
-    'pages/**/*.{js,ts,jsx,tsx}',
-    'app/**/*.{js,ts,jsx,tsx}',
-    'components/**/*.{js,ts,jsx,tsx}'
+    'src/app/**/*.{js,ts,jsx,tsx}',
+    'src/pages/**/*.{js,ts,jsx,tsx}'
   ],
-  project: ['**/*.{js,ts,jsx,tsx}'],
-  ignore: ['**/*.d.ts', '**/*.test.{js,ts,jsx,tsx}'],
+  project: ['src/**/*.{js,ts,jsx,tsx}'],
+  ignore: [
+    '**/*.d.ts',
+    '**/*.test.{js,ts,jsx,tsx}',
+    'src/components/ui/shadui/**' // Игнорируем всю папку с компонентами
+  ],
   next: {
-    entry: ['pages/**/*.{js,ts,jsx,tsx}', 'app/**/*.{js,ts,jsx,tsx}']
+    entry: ['src/pages/**/*.{js,ts,jsx,tsx}', 'src/app/**/*.{js,ts,jsx,tsx}']
   }
 }
 
